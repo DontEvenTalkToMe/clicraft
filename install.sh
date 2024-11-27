e9d1a15c0bd1bbabd67044749fd0853daba7be10
DontEvenTalkToMe <85612568+DontEvenTalkToMe@users.noreply.github.com>
2024-11-27 17:16:04 +0500

    Move script exec
    From /usr/bin to /usr/local/bin as per advice and info from u/Ulfnic on reddit(Thanks!)

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 mkdir -p $HOME/.config
 mkdir -p $HOME/.config/cli-craft; echo "Creating config folder..."; 
 chmod +x clicraft.sh 
-sudo mv clicraft.sh /usr/bin/clicraft; echo "Moving file to PATH..."
+sudo mv clicraft.sh /usr/local/bin/clicraft; echo "Moving file to PATH..."
 sudo mv clicraft.desktop /usr/share/applications
