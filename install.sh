a44c751b4df778fff1ec3fb9d41e53a35ff272dc
DontEvenTalkToMe <85612568+DontEvenTalkToMe@users.noreply.github.com>
2024-12-01 01:31:05 +0500

    Copy .desktop and exec
    instead of moving

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 mkdir -p $HOME/.config
 mkdir -p $HOME/.config/cli-craft; echo "Creating config folder..."; 
 chmod +x clicraft.sh 
-sudo mv clicraft.sh /usr/local/bin/clicraft; echo "Moving file to PATH..."
-sudo mv clicraft.desktop /usr/share/applications
+sudo cp clicraft.sh /usr/local/bin/clicraft; echo "Moving file to PATH..."
+sudo cp clicraft.desktop /usr/share/applications
